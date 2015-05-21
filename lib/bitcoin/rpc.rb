5309c5157c3858944b68cee6daeca9eb59ae1d16
nbabenko <babenko.nickolay@gmail.com>
2015-05-21 16:40:45 +0300

    HT-274 logs removed

--- lib/bitcoin/rpc.rb
+++ lib/bitcoin/rpc.rb
@@ -29,8 +29,6 @@ class Bitcoin::RPC
                     open_timeout: @open_timeout,
                     timeout: @read_timeout
     ) do |respdata, request, result|
-      Rails.logger.debug ("DEBUG: RPC request #{request.to_json}")
-      Rails.logger.debug ("DEBUG: RPC response #{respdata}")
       response = JSON.parse(respdata)
       raise Bitcoin::Errors::RPCError, response['error'] if response['error']
       response['result']
